b7210461c4b7cb29554b2da862a08005a136f4a4
Juan Pablo Ossa Zapata <ozjuanpa@gmail.com>
2021-09-24 21:04:49 -0500

    Password login fix

--- bootstrap_master.sh
+++ bootstrap_master.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-sed -i 's/#PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-
-echo "\nPermitRootLogin yes" >> /etc/ssh/sshd_config
-
-systemctl restart sshd
-
-echo "root:root" | chpasswd
-
 if [[ -d Kaladin ]]; then
     git -C Kaladin pull
 else
+    sed -i 's/#PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
+    echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+
+    systemctl restart sshd
+
+    echo "root:root" | chpasswd
     pacman -Syuu --noconfirm
     pacman -S --noconfirm git ansible sshpass
     git clone https://github.com/mkualquiera/Kaladin.git
