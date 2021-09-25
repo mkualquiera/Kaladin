89fc4c477a68dd69c949a5a46032ef9e6d6fecb9
Juan Pablo Ossa Zapata <ozjuanpa@gmail.com>
2021-09-24 19:19:02 -0500

    Add files

---
+++ boostrap_master.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pacman -Syuu --noconfirm
+pacman -S --noconfirm git ansible
+
+if [[ -d Kaladin ]]; then
+    git -C Kaladin pull
+else
+    git clone https://github.com/mkualquiera/Kaladin.git
+fi
+
+cd Kaladin
+
+ansible-playbook Kaladin/site.yml
